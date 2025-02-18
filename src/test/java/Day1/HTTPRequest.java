08102ac9655b37423cca0d8468db70e8f016e1b1
VennelaBhyravi.Vanam <VennelaBhyravi.Vanam@EGSD052.EGS.net>
2025-02-18 22:01:14 +0530

    Implemented the new Script

--- src/test/java/Day1/HTTPRequest.java
+++ src/test/java/Day1/HTTPRequest.java
@@ -1,6 +1,11 @@
 package Day1;
 
 import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import jdk.internal.net.http.common.Log;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -13,10 +18,59 @@ public class HTTPRequest {
 		.get("https://reqres.in/api/users/2")
 		//https://reqres.in/api/users?page=2
 		.then()
-		.statusCode(200)
+		.statusCode(200);
 		//.body("per_page", equalTo(6))
-		.log().all();
+		
 		 
 	}
+
+    private final String BASE_URL = "https://jsonplaceholder.typicode.com";
+
+    @Test
+    public void testGetRequest() {
+        Response response = given().get(BASE_URL + "/posts/1");
+        response.then().statusCode(200)
+        .log().all();
+        System.out.println(response.getBody().asString());
+    }
 	
+    @Test
+    public void testPostRequest() {
+        String requestBody = "{\"title\": \"Test\", \"body\": \"TestUser\", \"userId\": 2}";
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .post(BASE_URL + "/posts");
+
+        response.then().statusCode(201);
+        System.out.println(response.getBody().asString());
+    }
+
+    @Test
+    public void testPutRequest() {
+        String requestBody = "{\"id\": 1, \"title\": \"updated title\", \"body\": \"updated body\", \"userId\": 1}";
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .put(BASE_URL + "/posts/1");
+
+        response.then().statusCode(200);
+        System.out.println(response.getBody().asString());
+    }
+
+    @Test
+    public void testDeleteRequest() {
+        Response response = given().delete(BASE_URL + "/posts/1");
+        response.then().statusCode(200);
+        System.out.println(response.getBody().asString());
+    }
+    
+    @Test
+    public void testStatus304() {
+        Response response = given().get("https://httpstat.us/304");
+        response.then().statusCode(304);
+        System.out.println("Status 304: Not Modified");
+    }
 }
